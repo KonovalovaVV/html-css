5324bed7e6291a245d0a90aa7397b37bcb1f538d
KonovalovaVV <lerakonovalova1003@gmail.com>
2020-06-11 12:53:33 +0300

    Add webpack task.

---
+++ src/index.js
@@ -0,0 +1,23 @@
+ import _ from 'lodash';
+
+ import './css/menu.css';
+ import './css/main.css';
+ import './css/menu.css';
+ import './css/content.css';
+ import './css/footer.css';
+ import './css/reset.css';
+
+ import './img/gallery1.png';
+ import './img/gallery2.png';
+ import './img/gallery3.png';
+ import './img/gallery4.png';
+
+ import './img/profile1.png';
+ import './img/profile2.png';
+ import './img/profile3.png';
+ import './img/profile4.png';
+
+ import '@fortawesome/fontawesome-free/js/fontawesome';
+ import '@fortawesome/fontawesome-free/js/solid';
+ import '@fortawesome/fontawesome-free/js/regular';
+ import '@fortawesome/fontawesome-free/js/brands';
